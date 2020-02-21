c0fc1ec23481ce60d4fb5e7894b839067216de31
Michael DiLeo <m_dileosignup@live.com>
2020-02-21 16:08:08 -0600

    Michael's code. Added gitignore entries (#1)
    * Michael's code. Added gitignore entries
    
    * removed multiple sums

---
+++ Michael/AdventOfCodeChallenges.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCodeChallenges.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = new C2.Challenge().Run();
+            System.Console.WriteLine(result);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Michael/AdventOfCodeChallenges/C2/Challenge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCodeChallenges.C1;
+using MoreLinq;
+
+namespace AdventOfCodeChallenges.C2
+{
+    public sealed class Challenge
+    {
+        private readonly C1.Challenge _c1;
+
+        public Challenge()
+        {
+            _c1 = new C1.Challenge();
+        }
+
+        public long Run(long[] input = null) =>
+            (input ?? C1.Challenge.Inputs)
+                .SelectMany(i => MoreEnumerable.Unfold(i, x => _c1.FuelRequired(x), x => x > 0, x => x, x => x))
+                .Sum();
+    }
+
+}
