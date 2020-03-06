1f5c8910ab5f9ed17faf6cca5123548a23f77a9d
Timothy Ratliff <tratliff@amherst.com>
2020-03-06 15:01:59 -0600

    it was a valiant effort

--- tim/AOC2019.Day3/Program.cs
+++ tim/AOC2019.Day3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace AOC2019.Day3
 {
@@ -6,7 +9,60 @@ namespace AOC2019.Day3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputcodes1 = File.ReadAllText("list1.txt").Split(',');
+            string[] inputcodes2 = File.ReadAllText("list2.txt").Split(',');
+            List<string> codes1 = inputcodes1.ToList();
+            List<string> codes2 = inputcodes2.ToList();
+
+            GetManhattanDistance(codes1, codes2);
+            //Console.WriteLine(codes);
+        }
+
+        public static int GetDistance(string a, string b)
+        {
+            int aup = 0;
+            int adown = 0;
+            int aleft = 0;
+            int aright = 0;
+            int bup = 0;
+            int bdown = 0;
+            int bleft = 0;
+            int bright = 0;
+
+            Console.WriteLine($"list1 code: {a} | list2 code: {b}");
+            if (a.StartsWith("U"))            
+                aup = aup + Convert.ToInt32(a.Substring(1,a.Length));
+            if (a.StartsWith("D"))
+                adown = adown + Convert.ToInt32(a.Substring(1, a.Length)); 
+            if (a.StartsWith("L"))
+                aleft = aleft + Convert.ToInt32(a.Substring(1, a.Length)); 
+            if (a.StartsWith("R"))
+                aright = aright + Convert.ToInt32(a.Substring(1, a.Length)); 
+
+            if (b.StartsWith("U"))
+                bup = bup + Convert.ToInt32(b.Substring(1, b.Length));
+            if (b.StartsWith("D"))
+                bdown = bdown + Convert.ToInt32(b.Substring(1, b.Length));
+            if (b.StartsWith("L"))
+                bleft = bleft + Convert.ToInt32(b.Substring(1, b.Length));
+            if (b.StartsWith("R"))
+                bright = bright +Convert.ToInt32(b.Substring(1, b.Length));
+
+
+            return 1;
+        }
+
+        public static int GetManhattanDistance(List<string> codes1, List<string> codes2)
+        {
+            int result = 0;
+            for(int i = 0; i <= codes1.Count(); i++)
+            {
+                string code1 = codes1.ElementAt(i);
+                string code2 = codes2.ElementAt(i);
+
+            }
+
+            return result;
         }
     }
 }
